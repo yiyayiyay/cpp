109d6777065127d064de6debcf3dd7739881cdd0
yiyayiyay <18309816849@163.com>
2018-03-31 15:38:25 +0800

    for实现-100到100的累加

--- demo/demo/demo.cpp
+++ demo/demo/demo.cpp
@@ -7,7 +7,7 @@ int main()
 	int sum=0;
 	for (int val=-100;val<=100;++val)
 		sum +=val;
-	std::cout <<"将0到10的和为："<< sum <<std::endl ;
+	std::cout <<"将-100到100的和为："<< sum <<std::endl ;
 
 	/*while (val>=0)
 	{
